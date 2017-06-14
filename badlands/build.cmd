ecebc15937367767499c8458c6154cd9c7835dd9
Valentin Galea <valentingalea@gmail.com>
2017-06-14 17:44:21 +0100

    regen with Slidy2; intro background; new attribution section

--- badlands/build.cmd
+++ badlands/build.cmd
@@ -7,7 +7,7 @@ python ..\..\asciidoc\asciidoc.py ^
 -a language=cpp ^
 -a tabsize=4 ^
 --theme=volnitsky ^
---backend slidy ^
+--backend slidy2 ^
 -a duration=30 ^
 -a copyright="Valentin Galea" ^
 --verbose ^
