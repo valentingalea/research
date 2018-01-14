4710617fb40568db80a9563ccf92cf6c34ca40be
valentingalea <valentingalea@gmail.com>
2018-01-14 14:38:37 +0000

    add regular build script

---
+++ bitcoin/build.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+ROOT=$(pwd)
+
+cd ../tools/gnu-highlite/bin
+python ../../asciidoc/asciidoc.py \
+-a language=cpp \
+-a tabsize=4 \
+--theme=volnitsky \
+--backend slidy2 \
+-a duration=30 \
+-a copyright="Valentin Galea" \
+--verbose \
+--out-file=$ROOT/../docs/bitcoin/index.html \
+$ROOT/cpp_bitcoin.adoc
+
+cd $ROOT
