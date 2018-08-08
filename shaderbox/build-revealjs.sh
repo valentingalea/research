94cf0d5046474cf8c567e36ea87be9b56054b9bd
Valentin Galea <Valentin.Galea@splashdamage.com>
2018-08-08 18:50:50 +0100

    add (convoluted) support for plugins; integrated menu.js

--- shaderbox/build-revealjs.sh
+++ shaderbox/build-revealjs.sh
@@ -2,4 +2,8 @@
 
 node build.js
 
+# default way to enable plugins in asciidoctor doesn't seem to work
+# so I cheated and created my own mini mechanism
+sed -e '/dependencies:/ {' -e 'r plugins.js' -e 'd }' -i main.html
+
 cp main.html ../docs/shaderbox/index.html
